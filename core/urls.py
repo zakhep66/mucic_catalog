7e8aa9e48d686eda01401be6235c0c30284d7483
zakhep66 <sur.mikhael@gmail.com>
2023-02-10 17:10:10 +0300

    add jwt auth

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework_simplejwt.views import (
+	TokenObtainPairView,
+	TokenRefreshView,
+	TokenVerifyView,
+)
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/', include('app.urls')),
+	path('admin/', admin.site.urls),
+	path('api/v1/', include('app.urls')),
+
+	path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+	path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+	path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
