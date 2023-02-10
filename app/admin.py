579695a698cee779ba637c0d8c89f4fbc1832bef
zakhep66 <sur.mikhael@gmail.com>
2023-02-10 05:58:00 +0300

    import export

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 from .models import Composition, Category, Group, Artist, Role
 
-some_list = [Composition, Category, Group, Artist, Role]
+for i in [Category, Group, Artist, Role]:
+	admin.site.register(i)
 
-for i in some_list:
-    admin.site.register(i)
+
+class CompositionResource(resources.ModelResource):
+	class Meta:
+		model = Composition
+
+
+class CompositionAdmin(ImportExportModelAdmin):
+	resource_class = CompositionResource
+
+
+admin.site.register(Composition, CompositionAdmin)
--- core/settings.py
+++ core/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app.apps.AppConfig',
     'rest_framework',
+    'import_export',
 ]
 
 MIDDLEWARE = [
